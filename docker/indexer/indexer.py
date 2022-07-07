45ec5fc46e1461297d956ad2d794faf866937b27
Agassy <maor@duck.com>
2022-07-07 13:55:36 +0300

    Initial commit

---
+++ docker/indexer/indexer.py
@@ -0,0 +1,30 @@
+from retry import retry
+import pandas as pd
+import elasticsearch
+import elasticsearch.helpers
+import os
+
+@retry(elasticsearch.ConnectionError, max_delay=300, delay=5)
+def indexer():
+    es_client = elasticsearch.Elasticsearch(hosts=[{"host": "example_es"}])
+    index_name = "hero_" + str(os.environ['BUILD_NUMBER'])
+    number_of_shards = 1
+    
+    df_index = pd.read_csv("/app/data/indexme.csv", na_filter=False)
+    es_params = {
+        "index": index_name,
+        "body": {
+            "settings": {"index": {"number_of_shards": number_of_shards}}
+        },
+    }
+    if es_client.indices.exists(index=index_name):
+        es_client.indices.delete(index=index_name)
+    es_client.indices.create(**es_params)
+    elasticsearch.helpers.bulk(
+        es_client,
+        df_index.to_dict(orient="records"),
+        doc_type="_doc",
+        index=index_name,
+    )
+if __name__ == "__main__":
+    indexer()
